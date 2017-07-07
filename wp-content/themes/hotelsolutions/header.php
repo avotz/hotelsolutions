f76ee96d10be19a06ce15cff389792020556c1c8
Alonso Avotz <alonso@avotz.com>
2017-07-07 10:17:33 -0600

    se agrego favicon

--- wp-content/themes/hotelsolutions/header.php
+++ wp-content/themes/hotelsolutions/header.php
@@ -16,6 +16,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();  ?>/css/bootstrap.css">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri();  ?>/img/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();  ?>/img/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri();  ?>/img/favicons/favicon-16x16.png">
+<link rel="manifest" href="<?php echo get_template_directory_uri();  ?>/img/favicons/manifest.json">
+<link rel="mask-icon" href="<?php echo get_template_directory_uri();  ?>/img/favicons/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="theme-color" content="#ffffff">
 <?php wp_head(); ?>
  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
